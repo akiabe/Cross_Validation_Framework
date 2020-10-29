7248d028f051e3df057f01621d66105b378fa1c8
Akifumi Abe <akiabe0831@gmail.com>
2020-10-30 06:50:00 +0900

    update

--- src/train.py
+++ src/train.py
@@ -1,36 +1,51 @@
 import pandas as pd
-from sklearn import ensemble
+
+import lightgbm as lgb
 from sklearn import preprocessing
 from sklearn import metrics
 
 def run(fold):
     df = pd.read_csv("../input/train_folds.csv")
+
+    features = [
+        f for  in df.columns if f not in ("id", "target", "kfold")
+    ]
+
+    for col in features:
+        df.loc[:, col] = df[col].astype(str).fillna("NONE")
+
+    for col in features:
+        lbl = preprocessing.LabelEncoder()
+        lbl.fit(df[col])
+        df.loc[:, col] = lbl.transform(df[col])
+
     df_train = df[df.kfold != fold].reset_index(drop=True)
     df_valid = df[df.kfold == fold].reset_index(drop=True)
 
-    xtrain = df_train.drop(["id", "target", "kfold"], axis=1)
-    xvalid = df_valid.drop(["id", "target", "kfold"], axis=1)
-
-    xvalid = xvalid[xtrain.columns]
+    xtrain = df_train[features].values
+    xvalid = df_valid[features].values
 
     ytrain = df_train.target.values
     yvalid = df_valid.target.values
 
-    label_encoders = []
-    for c in xtrain.columns:
-        lbl = preprocessing.LabelEncoder()
-        lbl.fit(xtrain[c].values.tolist() + xvalid[c].values.tolist())
-        xtrain.loc[:, c] = lbl.transform(xtrain[c].values.tolist())
-        xvalid.loc[:, c] = lbl.transform(xvalid[c].values.tolist())
-        label_encoders.append((c, lbl))
-
-    clf = ensemble.RandomForestClassifier(n_jobs=-1, verbose=2)
+    clf = lgb.LGBMClassifier()
     clf.fit(xtrain, ytrain)
     pred = clf.predict_proba(xvalid)[:, 1]
-    #auc = metrics.roc_auc_score(yvalid, preds)
+    auc = metrics.roc_auc_score(yvalid, preds)
 
-    print(pred)
+    print(f"fold={fold}, auc={auc}")
+    #df_valid.loc[:, "lgb_pred"] = pred
+
+    #return df_valid[["id", "target", "kfold", "lgb_pred"]]
 
 if __name__ == "__main__":
+    #dfs = []
+
     for j in range(5):
         run(j)
+    #    temp_df = run(j)
+    #    dfs.append(temp_df)
+
+    #fin_valid_df = pd.concat(dfs)
+    #print(fin_valid_df.shape)
+    #fin_valid_df.to_csv("../model_preds/lgb.csv", index=False)
