4c770a3003a5ffc70675431de8f668cc25115b35
Akifumi Abe <akiabe0831@gmail.com>
2020-10-31 05:33:14 +0900

    update

--- src/cross_validation.py
+++ src/cross_validation.py
@@ -2,14 +2,11 @@ import pandas as pd
 from sklearn import model_selection
 
 """
-- binary classification
-- multi class classification
-- multi label classification
-- single column regression
-- multi column regression
-- holdout
+- Binary classification
+- Multilabel Classification
+- Regression Cross-validation
+- Hold-out Based validation
 """
-
 
 class CrossValidation:
     def __init__(
@@ -31,23 +28,19 @@ class CrossValidation:
 
         if self.shuffle is True:
             self.dataframe = self.dataframe.sample(frac=1).reset_index(drop=True)
-
         self.dataframe["kfold"] = -1
 
     def split(self):
         if self.problem_type in ("binary_classification", "multiclass_classification"):
             if self.num_targets != 1:
                 raise Exception("Invalid number of targets for this problem type")
+
             target = self.target_cols[0]
             unique_values = self.dataframe[target].nunique()
-
             if unique_values == 1:
                 raise Exception("Only one unique value found!")
             elif unique_values > 1:
-
-                kf = model_selection.StratifiedKFold(n_splits=self.num_folds,
-                                                     shuffle=False)
-
+                kf = model_selection.StratifiedKFold(n_splits=self.num_folds, shuffle=False)
                 for fold, (train_idx, val_idx) in enumerate(kf.split(X=self.dataframe, y=self.dataframe[target].values)):
                     self.dataframe.loc[val_idx, "kfold"] = fold
 
